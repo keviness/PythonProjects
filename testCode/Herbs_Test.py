21e0a25c0f2c2bea63e2591b5fde88e6281f33e4
keviness <kevin1737@163.com>
2020-07-21 22:44:26 +0800

    Update: testCode herb_test.py

--- testCode/Herbs_Test.py
+++ testCode/Herbs_Test.py
@@ -8,6 +8,7 @@ import pandas as pd
 from sklearn import svm
 import tkinter
 from tkinter import filedialog
+import openpyxl
 
 def getsvm(trainSet,tmpltSet,ptnset):
     clf = svm.SVC(kernel='linear', C = 1.0)
@@ -33,37 +34,41 @@ def getExcelFilePath():
         else:
             print("The choice is wrong, try again:")
 
-
-
-#TrainSet = 'TrainSet'
-#PtnSet = 'PtnSet'
-#TmpltSet = 'TmpltSet'
-
 #----- get trainSet, ptnSet and tmpltSet-----
 def getSetDatas(path, sheetName ,selectItemName):
     SourceData = pd.read_excel(path, sheet_name=sheetName)
     data = SourceData.set_index("中药名称")
-    #print("SourceData:", data)
     popItem = data.drop(index=selectItemName)
 
     #Train set
     TrainSetArray = np.array(popItem)
-    #print("popItem:", popItem)
-    #print("TrainSet:", TrainSetArray)
 
     #TmpltSet
     TmpltSet = popItem.index
     TmpltSetArray = np.array(TmpltSet)
-    #print("Ptnset:", PtnSet)
-    #print("TmpltSetArray:",TmpltSetArray)
 
     #PtnSet
     PtnSet = data.loc[selectItemName]
     PtnSetArray = np.array([PtnSet])
 
     result = (TrainSetArray, TmpltSetArray, PtnSetArray)
-    #print(result)
+
     return result
+
+def writeToExcelFile(path, resultDict):
+    keyList = []
+    valueList = []
+    for key in resultDict.keys():
+        keyList.append(key)
+        valueList.append(resultDict[key])
+    dataResult = pd.DataFrame({"待测药":keyList, "匹配药":valueList})
+    wb = openpyxl.load_workbook(path)
+    writer = pd.ExcelWriter(path, engine="openpyxl")
+    writer.book = wb
+    dataResult.to_excel(writer, sheet_name="result", columns=None)
+    writer.save()
+    writer.close()
+    print("Write to the excel file successfully!")
 
 def main():
     sheetName = 'GdaSet'
@@ -74,9 +79,10 @@ def main():
     except:
         print("Can\'t get the sourceData")
         return
-
+    resultDict = {}
     while True:
-        itemNames = np.array(data.index)
+        itemNames = list(data.index)
+        print("The herb items:".center(30, "-"))
         for index, value in enumerate(itemNames):
             print("{0:<10} {1:>10}".format(index, value))
         choice = input("Do you want to quit?(y/n)")[0]
@@ -84,23 +90,18 @@ def main():
             print("The done, bye~")
             break
         elif choice in ["n", "N"]:
-            selectItemName = input("Enter the selected item name:")
-            setDatas = getSetDatas(path, sheetName, selectItemName)
-            matchResult = getsvm(setDatas[0], setDatas[1], setDatas[2])
+            selectItemNum = eval(input("Enter the selected herb name:"))
+            while selectItemNum not in dict(enumerate(itemNames)).keys():
+                print(selectItemNum)
+                selectItemNum = eval(input("herb name input error!, Enter the selected herb name again:"))
+            setDatas = getSetDatas(path, sheetName, itemNames[selectItemNum])
+            matchResult = getsvm(setDatas[0], setDatas[1], setDatas[2])[0]
             print("The match result: \n", matchResult)
+            resultDict[itemNames[selectItemNum]] = matchResult
         else:
             print("The choice is wrong, try again!")
+    writeToExcelFile(path, resultDict)
 
 if __name__ == "__main__":
     main()
 
-'''
-df = SourceData["性"]
-print(df)
-writer = pd.ExcelWriter(path[0])
-SourceData.to_excel(writer, sheet_name="sourceData")
-df.to_excel(writer, sheet_name="select")
-writer.close()
-data = pd.read_excel(path[0])
-print("data:\n", data)
-'''
